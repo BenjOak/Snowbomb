0b871d2a40409cab1fb273ccef474fbffef6a792
bennie <benjowatson@gmail.com>
2023-12-09 03:26:04 +0000

    Added some assets

--- sketch.js
+++ sketch.js
@@ -1,8 +1,49 @@
+let snowTile, sprite1Still, sprite2Still;
+
+function preload() {
+    snowTile = loadImage("assets/Snow_Tile1.png");
+    sprite1Still = loadImage("assets/Sprite_1.png");
+    sprite2Still = loadImage("assets/Sprite_2_stoodstill.png");
+}
+
+class Sprite1 {
+    //movement
+    xDir;
+    yDir;
+    //position
+    x;
+    y;
+    //actions
+    still;
+    breathe;
+    walkLeft;
+    walkRight;
+    throwLeft;
+    throwRight;
+    build;
+
+    constructor() {
+        this.xDir = 0;
+        this.yDir = 0;
+        this.x = 900;
+        this.y = 500;
+        this.still = sprite1Still;
+    }
+}
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(1000, 1000);
 }
 
 function draw() {
-    background(150,0 ,0);
+    background(255);
+    imageMode(CENTER);
+    //image(Sprite1.still, Sprite1.x, Sprite1.y);
+    image(sprite1Still, width/2+20, height/2+2);
+    image(sprite2Still, width/2-20, height/2);
+}
+
+function ground() {
+
 }
 
